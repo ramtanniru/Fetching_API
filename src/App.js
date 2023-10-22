8f34e980e1e22c715cf6f1d5e0374cbc0d054515
ramtanniru <ramtanniru2@gmail.com>
2023-10-22 12:29:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Table from './components/Table'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-center fw-bold mt-5'>Dummy Data</h1>
+      <Table/>
+    </div>
+  );
+}
+
+export default App;
